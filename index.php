de7e7c698bd8636defc4e6a3966e43913845db0c
pedrohammes <predohammes@gmail.com>
2025-04-21 17:01:11 -0300

    estrutura inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <nav>
+        <a href="./index.php">Início</a>
+        <a href="./about.php">Sobre</a>
+        <a href="./products.php">Produtos</a>
+        <a href="./news.php">Novidades</a>
+        <a href="./contact.php">Contato</a>
+    </nav>
+    <h1>Empresa XYZ</h1>
+</body>
+</html>
