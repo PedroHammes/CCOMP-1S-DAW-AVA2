b1afb12d8997797ad4b1de9c24ca54a492b95901
pedrohammes <predohammes@gmail.com>
2025-04-27 10:54:26 -0300

    atualização das capas dos livros

--- database/books.sql
+++ database/books.sql
@@ -9,11 +9,13 @@ CREATE TABLE books (
 );
 
 -- População da tabela products
-INSERT INTO books (title, subtitle, price, in_stock)
+INSERT INTO books (title, subtitle, price, in_stock, img)
 VALUES
-('A Saga do Vampiro Rei', 'Bento', 29.90, 10),
-('A Saga do Vampiro Rei', 'A Bruxa Tereza', 39.90, 10),
-('A Saga do Vampiro Rei', 'Cantarzo', 49.90, 10),
-('O Senhor dos Anéis', 'A Sociedade do Anel', 39.90, 10),
-('O Senhor dos Anéis', 'As Duas Torres', 39.90, 10),
-('O Senhor dos Anéis', 'O Retorno do Rei', 39.90, 10);
+('As Crônicas de Gelo e Fogo', 'A Guerra dos Tronos ', 78.99, 10, 'https://m.media-amazon.com/images/I/91+1SUO3vUL._SY342_.jpg'),
+('As Crônicas de Gelo e Fogo', 'A Fúria dos Reis', 72.01, 10, 'https://m.media-amazon.com/images/I/51OxMUhiXwL._SY445_SX342_.jpg'),
+('As Crônicas de Gelo e Fogo', 'A Tormenta de Espadas', 94.93, 10, 'https://m.media-amazon.com/images/I/51lbSoFZYwL._SY445_SX342_.jpg'),
+('As Crônicas de Gelo e Fogo', 'O Festim dos Corvos', 71.90, 10, 'https://m.media-amazon.com/images/I/81PHekEOYhL._SY342_.jpg'),
+('As Crônicas de Gelo e Fogo', 'A Dança dos Dragões', 92.59, 10, 'https://m.media-amazon.com/images/I/91gIftSmvhL._SY342_.jpg')
+('O Senhor dos Anéis', 'A Sociedade do Anel', 39.90, 10, 'https://m.media-amazon.com/images/I/61VLT3aMviL._SY445_SX342_.jpg'),
+('O Senhor dos Anéis', 'As Duas Torres', 31.15, 10, 'https://m.media-amazon.com/images/I/61lW1iKenkL._SY445_SX342_.jpg'),
+('O Senhor dos Anéis', 'O Retorno do Rei', 32.11, 10, 'https://m.media-amazon.com/images/I/51Bsupv6jZL._SY445_SX342_.jpg');
